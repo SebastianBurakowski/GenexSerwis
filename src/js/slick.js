edadd41301bcd4522ec54d3ffcdd7be2e127c0be
Sebastian Burakowski <s-burakowski@wp.pl>
2025-01-03 15:07:47 +0100

    create opinions bar

--- src/js/slick.js
+++ src/js/slick.js
@@ -1,18 +1,19 @@
 $(document).ready(function () {
     $('.opinions__box').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 3300,
-        arrows: false,
-        mobileFirst: true,
-        responsive: [{
-            breakpoint: 770,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1
+        slidesToShow: 2,       // Wyświetlaj 2 slajdy naraz
+        autoplaySpeed: 4000,
+        slidesToScroll: 1,     // Przewijaj o 1 slajd
+        arrows: false,          // Pokazuj strzałki
+        dots: false,            // Dodaj nawigację w formie kropek
+        autoplay: true,       // Opcjonalnie: automatyczne przesuwanie
+        responsive: [          // Responsywne ustawienia
+            
+            {
+                breakpoint: 992,  // Dla ekranów mniejszych niż 768px
+                settings: {
+                    slidesToShow: 1, // Wyświetlaj tylko 1 slajd
+                }
             }
-        }],
-
+        ]
     });
-})
+});
